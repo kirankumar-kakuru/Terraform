24bd720486a9a33a2e2f0bc8dfc0745b442f822d
Kiran <kiranwls6@gmail.com>
2026-02-23 20:06:06 -0800

    test_1

--- COUNT_LOOPS/ec2.tf
+++ COUNT_LOOPS/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
     #count = 3
     count = length(var.instance)
     ami           = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.micr0"
+    instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
     tags = {
